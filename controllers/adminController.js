3cfbd88ef9b93e00eda2ebf01638fbf458f572a5
rubiyadav <rubiflyeis@gmail.com>
2023-09-21 19:12:19 +0530

    update

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const advertisement = require('../model/Advertisement/advertisement');
 const banner = require('../model/Banner/banner');
 const bookCabs = require('../model/BookCabs/bookCabs');
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const advertisement = require('../model/Advertisement/advertisement');
 const banner = require('../model/Banner/banner');
 const company = require('../model/company/company');
